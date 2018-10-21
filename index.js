05df3ea5d002fa46b2e55142a15972a42586e33b
Ir1d <sirius.caffrey@gmail.com>
2018-10-21 21:27:25 +0800

    fix:
    update

--- index.js
+++ index.js
@@ -1,50 +1,51 @@
-bin/linux_x64/clang-format
+"use strict";
 
-'use strict'
-
-var visit = require('unist-util-visit')
-var fs = require('fs')
-spawn = require('child_process').spawn
+var visit = require("unist-util-visit");
+var fs = require("fs");
+var spawnSync = require("child_process").spawnSync;
+var os = require("os");
+var path = require("path");
 
 function getNativeBinary() {
   let nativeBinary;
 
-  if (os.platform() === 'win32') {
-    nativeBinary = path.join(`${__dirname}`, `../clang-format/bin/win32/clang-format.exe`);
+  if (os.platform() === "win32") {
+    nativeBinary = path.join(
+      `${__dirname}`,
+      `../clang-format/bin/win32/clang-format.exe`
+    );
   } else {
-    nativeBinary = path.join(`${__dirname}`, `../clang-format/bin/${os.platform()}_${os.arch()}/clang-format`);
+    nativeBinary = path.join(
+      `${__dirname}`,
+      `../clang-format/bin/${os.platform()}_${os.arch()}/clang-format`
+    );
   }
 
   if (!fs.existsSync(nativeBinary)) {
-    const message = 'This module doesn\'t bundle the clang-format executable for your platform. ' +
+    const message =
+      "This module doesn't bundle the clang-format executable for your platform. " +
       `(${os.platform()}_${os.arch()})\n` +
-      'Consider installing it with your native package manager instead.\n';
+      "Consider installing it with your native package manager instead.\n";
     throw new Error(message);
   }
   return nativeBinary;
 }
 
-
 function visitor(node) {
-  let nativeBinary = getNativeBinary()
-  if (node.type == 'code' || node.type == 'inlineCode') {
-    if (node.lang.toLowerCase() == 'c++' || node.lang.toLowerCase() == 'cpp') {
-      var child = spawnSync(nativeBinary);
-
-      child.stdin.setEncoding('utf-8');
-      child.stdout.pipe(process.stdout);
-
-      child.stdin.write(node.value);
-
-      child.stdin.end();
-
-      node.value = format(child.stdout)
+  let nativeBinary = getNativeBinary();
+  if (node.type == "code" || node.type == "inlineCode") {
+    if (
+      node.lang &&
+      (node.lang.toLowerCase() == "c++" || node.lang.toLowerCase() == "cpp")
+    ) {
+      var child = spawnSync(nativeBinary, { input: node.value });
+      node.value = child.stdout;
     }
   }
 }
 
 module.exports = function attacher() {
   return function transformer(tree, file) {
-    visit(tree, visitor)
-  }
-}
+    visit(tree, visitor);
+  };
+};
