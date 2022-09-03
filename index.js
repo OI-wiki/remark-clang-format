0f184790f3d18243f68e1e1e06cf1b307800354d
Nathan Du <nathandu@outlook.com>
2022-09-03 08:57:13 +0800

    move language-unrelated conditions to upper level
    The language check has been converted to a switch statement for easier
    maintenance.
    
    Also no longer checks inlineCode nodes because the language cannot be
    specified.

--- index.js
+++ index.js
@@ -4,28 +4,32 @@ import { visit } from 'unist-util-visit';
 import { spawnSync as spawnSync } from 'child_process';
 
 function visitor(node) {
-  if (node.type == 'code' || node.type == 'inlineCode') {
-  // if (node.type == 'inlineCode') {
-    if (
-      node.lang &&
-      (node.lang.toLowerCase() == 'c++' ||
-        node.lang.toLowerCase() == 'cpp' ||
-        node.lang.toLowerCase() == 'c') &&
-      (!node.meta || !node.meta.includes('nolint')) &&
-      (node.value && (!node.value.includes('--8<--')))
-    ) {
-      const child = spawnSync('clang-format', { input: node.value });
-      if (child.stderr.toString()) {
-        console.warn("[remark-clang-format] stderr: ", child.stderr.toString());
-      }
-      if (!child.stdout) {
-        console.warn("[remark-clang-format] empty stdout");
-        console.warn("[remark-clang-format] original code: ", node.value);
-        console.warn("[remark-clang-format] child info", child);
-        // node value left untouched
-      } else {
-        node.value = child.stdout;
-      }
+  if (
+    node.type == 'code' &&
+    node.lang &&
+    (!node.meta || !node.meta.includes('nolint')) && node.value &&
+    !node.value.includes('--8<--')
+  ) {
+    switch (node.lang) {
+      case 'c++':
+      case 'cpp':
+      case 'c':
+        const child = spawnSync('clang-format', { input: node.value });
+        if (child.stderr.toString()) {
+          console.warn(
+            '[remark-clang-format] stderr: ',
+            child.stderr.toString(),
+          );
+        }
+        if (!child.stdout) {
+          console.warn('[remark-clang-format] empty stdout');
+          console.warn('[remark-clang-format] original code: ', node.value);
+          console.warn('[remark-clang-format] child info', child);
+          // node value left untouched
+        } else {
+          node.value = child.stdout;
+        }
+        break;
     }
   }
 }
