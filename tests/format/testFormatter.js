05f4685c018f11186b5ef86109ba0fe6d85ecfcc
Nathan Du <nathandu@outlook.com>
2022-09-21 22:35:07 +0800

    move base formatter tests

--- tests/format/testFormatter.js
+++ tests/format/testFormatter.js
@@ -2,12 +2,12 @@ import test from 'ava';
 import { promises } from 'fs';
 import remark from 'remark';
 
-import de from '../index.js';
+import de from '../../index.js';
 
 async function T(num) {
-  test(`test case #${num}`, async (t) => {
-    const infile = await promises.readFile(`tests/${num}.in.md`);
-    const outfile = await promises.readFile(`tests/${num}.out.md`);
+  test(`format file #${num}`, async (t) => {
+    const infile = await promises.readFile(`tests/format/${num}.in.md`);
+    const outfile = await promises.readFile(`tests/format/${num}.out.md`);
     remark()
       .use(de)
       .process(infile, function (err, res) {
