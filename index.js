e8c78f0fa4162e51655b29bd39f04267ef65b5a3
mgt <mgt@oi-wiki.org>
2024-05-08 00:13:56 +0800

    feat: allow controlling math behavior

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 import formatCode from './code/format-code.js';
 import formatMath from './math/format-math.js';
 
-export default function remarkCodeFormat() {
+export default function remarkCodeFormat(options) {
   return function transformer(tree) {
     formatCode(tree);
-    formatMath(tree);
+    if (options && options.math) {
+      formatMath(tree);
+    }
   };
 }
